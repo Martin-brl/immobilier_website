7aa95430924386e14add4b1934db6535997801a6
stanprr <131998832+stanprr@users.noreply.github.com>
2024-06-02 16:44:07 +0200

    Add files via upload

--- Projet_Piscine_2/connexion.php
+++ Projet_Piscine_2/connexion.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+session_start();   
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -10,12 +10,47 @@ session_start();
     <link rel="stylesheet" href="styles.css">
      <!-- Leaflet CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+     <style>
+        .user-info {
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+            border: 1px solid #ddd;
+            padding: 20px;
+            border-radius: 5px;
+            max-width: 400px;
+             margin: 150px auto 20px auto; /* Ajout de margin-top de 90px */
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .user-info p {
+            margin: 10px 0;
+        }
+
+        .user-info .label {
+            font-weight: bold;
+            color: #333;
+        }
+
+        .user-info p:nth-child(odd) {
+            background-color: #f1f1f1;
+            padding: 10px;
+            border-radius: 3px;
+        }
+
+        .user-info p:nth-child(even) {
+            background-color: #ffffff;
+            padding: 10px;
+            border-radius: 3px;
+        }
+    </style>
 </head>
 <body>
+
     <header>
         <div class="header-left">
             <img src="logo.png" alt="Omnes Immobilier Logo" class="logo">
             <img src="titre.jpg" alt="Titre" class="titre-image">
+
         </div>
 
         <nav class="header-right">
@@ -30,6 +65,20 @@ session_start();
     </header>
 
 <!-- ************************************************************************************************************************-->
+
+<?php
+echo '<section class="user-info">';
+echo '<p><span class="label">Prénom:</span> ' . @$_SESSION['user_prenom'] . '</p>';
+echo '<p><span class="label">Nom:</span> ' . @$_SESSION['user_nom'] . '</p>';
+echo '<p><span class="label">Email:</span> ' . @$_SESSION['user_email'] . '</p>';
+echo '<p><span class="label">Adresse:</span> ' . @$_SESSION['user_adresse'] . '</p>';
+echo '<p><span class="label">Type:</span> ' . @$_SESSION['user_type'] . '</p>';
+echo '<p><span class="label">Téléphone:</span> ' . @$_SESSION['user_telephone'] . '</p>';
+echo '</section>';
+?>
+
+<!-- ************************************************************************************************************************-->
+
 <div class="wrapper">
         <section class="votre-compte-container">
             <h2>Connexion :</h2>
@@ -43,9 +92,6 @@ session_start();
                 
                 
                 <button type="submit" class="votre-compte-button">Se connecter</button>
-            </form>
-            <form method="POST" action="deconnexion.php">
-                <button type="submit" class="votre-compte-button">Se déconnecter</button>
             </form>
         </section>
     </div>
